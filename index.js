e0eb3a675fc2aa4650a20b35c7770f4a7547edb4
D113G <fbrccrrrr@gmail.com>
2024-11-19 00:10:03 +0500

    Update Calculator 1.2

--- index.js
+++ index.js
@@ -1,18 +1,54 @@
 console.log('hello')
-var buttonElement = document.getElementById('button')
+var buttonElementP = document.getElementById('buttonP');
+var buttonElementMi = document.getElementById('buttonMi')
+var buttonElementMu = document.getElementById('buttonMu')
+var buttonElementDi = document.getElementById('buttonDi')
 var inputElement1 = document.getElementById('number1');
 var inputElement2 = document.getElementById('number2');
-var result=document.getElementById('result')
+var result = document.getElementById('result')
 
-buttonElement.addEventListener('click', function (event) {
+buttonElementP.addEventListener('click', function (event) {
 
     var value1 = inputElement1.value;
     var value2 = inputElement2.value;
-var n1=parseFloat(value1)
-var n2=parseFloat(value2)
-var sum=n1+n2;
-result.innerText=sum
-console.log(sum)
+    var n1 = parseFloat(value1)
+    var n2 = parseFloat(value2)
+    var sum = n1 + n2;
+    result.innerText = sum
+    console.log(sum)
+})
+
+buttonElementMi.addEventListener('click', function (event) {
+
+    var value1 = inputElement1.value;
+    var value2 = inputElement2.value;
+    var n1 = parseFloat(value1)
+    var n2 = parseFloat(value2)
+    var sum = n1 - n2;
+    result.innerText = sum
+    console.log(sum)
+})
+
+buttonElementMu.addEventListener('click', function (event) {
+
+    var value1 = inputElement1.value;
+    var value2 = inputElement2.value;
+    var n1 = parseFloat(value1)
+    var n2 = parseFloat(value2)
+    var sum = n1 * n2;
+    result.innerText = sum
+    console.log(sum)
+})
+
+buttonElementDi.addEventListener('click', function (event) {
+
+    var value1 = inputElement1.value;
+    var value2 = inputElement2.value;
+    var n1 = parseFloat(value1)
+    var n2 = parseFloat(value2)
+    var sum = n1 / n2;
+    result.innerText = sum
+    console.log(sum)
 })
 
 
